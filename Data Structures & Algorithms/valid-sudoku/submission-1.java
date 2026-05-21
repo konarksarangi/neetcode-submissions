dfee9e351468d20d265e465e90b996391e98aaf2
konarksarangi <konark.sarangi@gmail.com>
2026-05-21 07:33:39 -0700

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean [][] rows = new boolean [9][9];
+        boolean [][] columns = new boolean [9][9];
+        boolean [][] boxes = new boolean [9][9];
+
+        for (int r=0;r<9;r++){
+            for (int c=0;c<9;c++){
+                if (board[r][c] == '.'){
+                    continue;
+                }
+
+                int num = board[r][c] - '1';
+
+                int boxIndex = (r/3) *3 + (c/3);
+
+                if (rows[r][num] || columns[c][num] || boxes[boxIndex][num]){
+                    return false;
+                }
+                rows[r][num] = true;
+                columns[c][num] = true;
+                boxes[boxIndex][num] = true;
+            }
+        }
+        return true;
+    }
+}
