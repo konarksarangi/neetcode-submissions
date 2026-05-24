dccbb9e4c8d100838bdfbce6323ae33279f3d1e5
konarksarangi <konark.sarangi@gmail.com>
2026-05-24 07:11:36 -0700

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+        public int[][] merge(int[][] intervals) {
+        if (intervals.length <1){
+            return intervals;
+        }
+        LinkedList<int[]> finalList  = new LinkedList<>();
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+
+        for (int[] interval : intervals){
+            if (finalList.isEmpty() || interval[0] > finalList.getLast()[1]){
+                finalList.add(interval);
+            }
+            else{
+                finalList.getLast()[1]=Math.max(finalList.getLast()[1],interval[1]);
+            }
+    
+        }
+        return finalList.toArray(new int[finalList.size()][]);
+        
+    }
+}
