49a80bf1b75177210d05a3e4dfb29dbe259d2e67
konarksarangi <konark.sarangi@gmail.com>
2026-06-24 11:45:17 -0700

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        String encodeDelimiter="#";
+        StringBuilder encodedString = new StringBuilder();
+        for(String str:strs){
+            encodedString.append(str.length());
+            encodedString.append(encodeDelimiter);
+            encodedString.append(str);
+        }
+        return encodedString.toString();
+    }
+
+    public List<String> decode(String str) {
+        final char delimiter='#';
+        List<String> result = new ArrayList<>();
+        int i=0;
+        int j=0;
+        while (i <str.length()){
+            if(str.charAt(i) == delimiter){
+                int length = Integer.parseInt(str.substring(j,i));
+                result.add(str.substring(i+1,i+1+length));
+                j=i+1+length;
+                i+=length;
+            }
+            i++;
+        }
+        return result;
+    }
+}
