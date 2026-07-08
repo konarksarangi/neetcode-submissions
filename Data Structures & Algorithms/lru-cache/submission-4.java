db4190b8c400875c68f0fc83fffca6e60162f5d9
konarksarangi <konark.sarangi@gmail.com>
2026-07-08 11:21:41 -0700

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.java
@@ -0,0 +1,25 @@
+class LRUCache {
+    private final int capacity;
+    private final LinkedHashMap<Integer,Integer> storageMap;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.storageMap = new LinkedHashMap<>(capacity,0.75f,true);
+    }
+
+    public int get(int key) {
+        return  this.storageMap.getOrDefault(key,-1);
+
+    }
+
+    public synchronized void  put(int key, int value) {
+        if (storageMap.containsKey(key)) {
+            this.storageMap.put(key, value);
+        }else{
+            if (this.storageMap.size() == this.capacity) {
+                this.storageMap.pollFirstEntry();
+            }
+            this.storageMap.put(key, value);
+        }
+    }
+}
