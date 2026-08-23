57f4031c878250b13036f4eab36f1916860810ab
konarksarangi <konark.sarangi@gmail.com>
2026-08-23 08:20:43 -0700

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int l=0,r=heights.length-1;
+        int area=0,maxArea=0;
+
+        while (l<r){
+            area=Math.min(heights[l],heights[r]) * (r-l);
+            maxArea=Math.max(area,maxArea);
+            if(heights[l]<heights[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return maxArea;
+    }
+}
